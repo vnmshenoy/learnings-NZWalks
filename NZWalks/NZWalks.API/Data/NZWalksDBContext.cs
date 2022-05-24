776fa23ba80786a69f78dcdd2e372e9b91f5f617
manju <vnmshenoy@gmail.com>
2022-05-24 14:48:52 +1000

    end of section2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> options) : base(options)
+        {
+
+        }
+        /* Below are 3 properties of DbSet which basically tells
+           EF that please create "Regions" table for us if it doesn't exist
+           in the DB. So by giving the property like this (like DbSet),we can now use
+           the "NZWalksDBContext" so that it can query/persist the data to the region table that
+           EF core will create as part of migrations (we will see migrations later)
+        */
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDiffcultyId { get; set; }
+
+        //Naviagation Property
+        public Region Region { get; set; }  
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
